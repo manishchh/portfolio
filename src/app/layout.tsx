351ec0b982b5d74a35a470b6e3d09686dbee7f0a
manishchh <chhetrim901@gmail.com>
2026-03-29 13:57:53 +1030

    chore: apply custom global theme and force dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +26,14 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn(
+        "h-full",
+        "dark",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans"
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
